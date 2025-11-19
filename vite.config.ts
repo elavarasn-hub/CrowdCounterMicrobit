3d3af5ee8a420e496d62fc856cd6afc005f79c4f
elavarasn-hub <toelavarasan@gmail.com>
2025-11-19 16:01:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// IMPORTANT: set base to your repo name so GitHub Pages finds assets
 export default defineConfig({
+  base: '/CrowdCounterMicrobit/',
   plugins: [react()],
-  base: "/CrowdCounterMicrobit/",
-});
-
+})
